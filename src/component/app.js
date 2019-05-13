0560f7a4c685dfce8cb4a40c3292b1e5adea5d90
David Marchante <cdmarchante@gmail.com>
2019-05-13 13:59:56 -0700

    added basic styling

--- src/component/app.js
+++ src/component/app.js
@@ -4,30 +4,24 @@ import '../scss/core.scss';
 export default function App() {
   return (
     <Fragment>
-      <div className="stylish">
-        Hello World!
-      </div>
-      <div className="slabo">
-        Hello World!
-      </div>
-      <div className="crimson">
-        Hello World!
-      </div>
-      <div className="forum">
-        Hello World!
-      </div>
-      <div className="cinzel">
-        Hello World!
-      </div>
-      <div className="mizra">
-        Hello World!
-      </div>
-      <div className="matmorphous">
-        Hello World!
-      </div>
-      <div className="unifra">
-        Hello World!
-      </div>
+      <body>
+        <header>
+          <h1 className="cinzel">
+            Dungeon Data
+          </h1>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>About Game</li>
+              <li>Monsters</li>
+            </ul>
+          </nav>
+        </header>
+        <p className="montserrat">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et pharetra pharetra massa massa ultricies. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Ultrices dui sapien eget mi proin sed libero enim sed. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Sagittis purus sit amet volutpat consequat mauris. Posuere urna nec tincidunt praesent semper. At erat pellentesque adipiscing commodo elit at imperdiet. Aliquam malesuada bibendum arcu vitae elementum. Blandit turpis cursus in hac habitasse platea dictumst quisque.
+        </p>
+      </body>
     </Fragment>
   );
 }
