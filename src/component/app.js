48d7899aff7ebfb9ec30eb17f92eb32a8bbbbbbf
David Marchante <cdmarchante@gmail.com>
2019-05-13 13:01:18 -0700

    test fonts

--- src/component/app.js
+++ src/component/app.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 export default function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Fragment>
+      <div className="stylish">
+        Hello World!
+      </div>
+      <div className="slabo">
+        Hello World!
+      </div>
+      <div className="crimson">
+        Hello World!
+      </div>
+      <div className="forum">
+        Hello World!
+      </div>
+      <div className="cinzel">
+        Hello World!
+      </div>
+      <div className="mizra">
+        Hello World!
+      </div>
+      <div className="matmorphous">
+        Hello World!
+      </div>
+      <div className="unifra">
+        Hello World!
+      </div>
+    </Fragment>
   );
 }
