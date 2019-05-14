8501ccbc8ee96c6aa321db9faec375dfe9573d27
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-14 15:17:19 -0700

    Displaying monster features

--- src/component/monsters.js
+++ src/component/monsters.js
@@ -26,12 +26,23 @@ export default class Monsters extends Component {
   render () {
     let images = [];
 
-    this.state.response.map(el => images.push({name: el.name, path: `characters/${el.name}.jpg`}));
+    this.state.response.map(el => images.push({name: el.name, path: `characters/${el.name}.jpg`, size: el.size, type: el.type, armor_class: el.armor_class, hit_points: el.hit_points, hit_dice:el.hit_dice, challenge_rating: el.challenge_rating}));
 
     return (
       <Fragment>
         <div className="monsterImages">
-            {images.map(el => <div><h2>{el.name}</h2><img src={el.path}></img></div>)}
+            {images.map(el => <div className="monsterCard">
+            <img src={el.path} alt="monsters"></img>
+              <ul>
+                <li><h3>{el.name}</h3></li>
+                <li>Size: {el.size}</li>
+                <li>Type: {el.type}</li>
+                <li>Armor Class: {el.armor_class}</li>
+                <li>Hit Points{el.hit_points}</li>
+                <li>Hit Dice{el.hit_dice}</li>
+                <li>Challenge Rating{el.challenge_rating}</li>
+              </ul>
+            </div>)}
         </div>
       </Fragment>
     );
