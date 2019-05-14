9a51b46d2739a0116ecd0518966413ffe3601475
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-14 10:34:07 -0700

    working on about us

--- src/component/who.js
+++ src/component/who.js
@@ -4,9 +4,11 @@ class Who extends React.Component {
   render () {
     return (
       <Fragment>
-          <img src={this.props.image} alt="Creaters"/>
-          <h2>{this.props.name}</h2>
-          <p>{this.props.description}</p>
+        <ul>
+          <li><img src={this.props.image} alt="Creaters"/></li>
+          <li><h2>{this.props.name}</h2></li>
+          <li><p>{this.props.description}</p></li>   
+        </ul>
       </Fragment>
     );
   };
