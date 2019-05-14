e6a411d3f8ede561f0ca125ed653c562420349ca
David Marchante <cdmarchante@gmail.com>
2019-05-14 16:58:00 -0700

    add flip[ capability

--- src/component/monsters.js
+++ src/component/monsters.js
@@ -34,8 +34,28 @@ export default class Monsters extends Component {
               <option>Size</option>x
               <option>Type</option>
               <option>Armor Class</option>
-            </select></div>
+            </select>
+          </div>
           {images.map(el => <div className="monsterCard">
+              <div className="monsterCard-inner">
+                <div className="monsterCard-front">
+                  <img src={el.path} alt="monsters"></img>
+                  <h3>{el.name}</h3>
+                </div>
+                <div className="monsterCard-back">
+                  <ul>
+                    <li><h3>{el.name}</h3></li>
+                    <li>Size: {el.size}</li>
+                    <li>Type: {el.type}</li>
+                    <li>Armor Class: {el.armor_class}</li>
+                    <li>Hit Points{el.hit_points}</li>
+                    <li>Hit Dice{el.hit_dice}</li>
+                    <li>Challenge Rating{el.challenge_rating} </li>
+                  </ul>
+                </div>
+              </div>
+            </div>)}
+          {/* {images.map(el => <div className="monsterCard">
             <img src={el.path} alt="monsters"></img>
               <ul>
                 <li><h3>{el.name}</h3></li>
@@ -46,7 +66,7 @@ export default class Monsters extends Component {
                 <li>Hit Dice{el.hit_dice}</li>
                 <li>Challenge Rating{el.challenge_rating}</li>
               </ul>
-          </div>)}
+          </div>)} */}
         </div>
       </Fragment>
     );
